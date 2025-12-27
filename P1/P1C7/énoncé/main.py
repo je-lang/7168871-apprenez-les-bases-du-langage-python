5ac3cd7b736fdb690f95fef7f17619b98fa6fa4a
je-lang <writetojerome@gmail.com>
2025-12-27 10:23:21 +0100

    Résolution d'un exercice sur les dictionnaires en Python.

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,7 @@
 # Écrivez votre code ici !
+fruits = {"pomme" : "rouge", "banane" : "jaune", "orange" : "orange"}
+fruits["kiwi"] = "vert"
+couleur_banane = fruits["banane"]
+fruits["pomme"] = "vert"
+fruits.pop("banane")
+print(fruits)
